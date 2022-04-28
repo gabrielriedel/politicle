eef72620948cec8f921aaed00fc17005e972f633
Lance Riedel <lanceriedel@gmail.com>
2022-04-27 21:50:34 -0500

    notarizing scripts

---
+++ checknotarizing.sh
@@ -0,0 +1,3 @@
+xcrun altool --notarization-info "aa8cb89c-4860-4f55-ad42-c44f312ee331" \
+  --username lanceariedel@icloud.com \
+  --password "nsjj-gbfm-koit-ptdp"
---
+++ notaraize.sh
@@ -0,0 +1,4 @@
+xcrun altool -t osx -f Politicle-1.0.dmg \
+  --primary-bundle-id com.griedel.wordy.Main --notarize-app \
+  --username lanceariedel@icloud.com \
+  --password "nsjj-gbfm-koit-ptdp"
